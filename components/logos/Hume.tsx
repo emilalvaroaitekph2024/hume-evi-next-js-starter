8350b68fb092fd8d987797966fb33e2f9f9715d9
Emil Alvaro AitekPH <ceo@aitekph.com>
2024-07-06 08:53:04 +0800

    Update Hume.tsx

--- components/logos/Hume.tsx
+++ components/logos/Hume.tsx
@@ -10,24 +10,6 @@ const HumeLogo: FC<HumeLogoProps> = (props) => {
 
   return (
     <svg
-      width="106"
-      height="25"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 106 25"
-      {...props}
-    >
-      <linearGradient
-        id={gradientId}
-        gradientUnits="userSpaceOnUse"
-        x1="21.58783"
-        y1="6.94375"
-        x2="22.83713"
-        y2="11.14995"
-        gradientTransform="matrix(1 0 0 -1 1.324843e-07 23.88861)"
-      >
-        <stop offset="0.2656" stopColor="#FFB7B2" />
-        <stop offset="0.5781" stopColor="#AB9EFC" />
-      </linearGradient>
     </svg>
   );
 };
